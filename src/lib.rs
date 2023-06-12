f112b5de654f8796e451896fce7b28748dfeec96
Dom Dwyer <dom@itsallbroken.com>
2023-06-12 20:56:58 +0200

    docs: update copyright year

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
     missing_docs
 )]
 
-//   Copyright 2021 Dominic Dwyer (dom@itsallbroken.com)
+//   Copyright 2023 Dominic Dwyer (dom@itsallbroken.com)
 //
 //   Licensed under the Apache License, Version 2.0 (the "License"); you may not
 //   use this file except in compliance with the License. You may obtain a copy
