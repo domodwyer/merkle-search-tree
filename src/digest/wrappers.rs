117f2682740c69c36396606f8338bca1f1435783
Dom Dwyer <dom@itsallbroken.com>
2023-06-17 23:19:54 +0200

    feat: base64 hash Display impl
    Optionally support rendering hash digests as base64 strings via a
    Display impl.

--- src/digest/wrappers.rs
+++ src/digest/wrappers.rs
@@ -26,22 +26,10 @@ impl RootHash {
     }
 }
 
-/// Type wrapper over a [`Digest`] of a tree value, for readability / clarity /
-/// compile-time safety.
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub struct ValueDigest<const N: usize>(Digest<N>);
-
-impl<const N: usize> std::ops::Deref for ValueDigest<N> {
-    type Target = Digest<N>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<const N: usize> ValueDigest<N> {
-    pub(crate) const fn new(value: Digest<N>) -> Self {
-        Self(value)
+#[cfg(feature = "digest_base64")]
+impl std::fmt::Display for RootHash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
     }
 }
 
@@ -63,5 +51,38 @@ impl std::ops::Deref for PageDigest {
 impl PageDigest {
     pub(crate) const fn new(value: Digest<16>) -> Self {
         Self(value)
+    }
+}
+
+#[cfg(feature = "digest_base64")]
+impl std::fmt::Display for PageDigest {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+/// Type wrapper over a [`Digest`] of a tree value, for readability / clarity /
+/// compile-time safety.
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct ValueDigest<const N: usize>(Digest<N>);
+
+impl<const N: usize> std::ops::Deref for ValueDigest<N> {
+    type Target = Digest<N>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<const N: usize> ValueDigest<N> {
+    pub(crate) const fn new(value: Digest<N>) -> Self {
+        Self(value)
+    }
+}
+
+#[cfg(feature = "digest_base64")]
+impl<const N: usize> std::fmt::Display for ValueDigest<N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
     }
 }
